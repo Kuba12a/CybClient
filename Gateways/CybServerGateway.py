3df2a58fe3ada5d26f24c858a20734c678343790
kalus <kaluski1212@wp.pl>
2022-12-04 12:56:29 +0100

    Add generating first code for agent to CybServerGateway

--- Gateways/CybServerGateway.py
+++ Gateways/CybServerGateway.py
@@ -3,19 +3,33 @@ import json
 import requests
 
 HTTP_PREFIX = "http://"
-HOST = "192.168.1.51:80/internal/downloadFile"
+HOST = "192.168.1.51:80/internal"
 
 
 class DownloadFileFromAgentInputType(BaseModel):
-    host: str
+    ip_address: str
     file_path: str
+
+
+class GenerateFirstCodeForAgentInputType(BaseModel):
+    ip_address: str
 
 
 def download_file_from_agent(input: DownloadFileFromAgentInputType):
 
     data = json.dumps(input.__dict__)
 
-    response = requests.post(url=HTTP_PREFIX+input.host, data=data)
+    response = requests.post(url=HTTP_PREFIX+HOST+"/downloadFile", data=data)
+
+    status_code = response.status_code
+    return status_code
+
+
+def generate_first_code_for_agent(input: GenerateFirstCodeForAgentInputType):
+
+    data = json.dumps(input.__dict__)
+
+    response = requests.post(url=HTTP_PREFIX+HOST+"/generateAgentCode", data=data)
 
     status_code = response.status_code
     return status_code
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import Gateways.CybServerGateway as cybServerGateway
 
 if __name__ == '__main__':
-    cybServerGateway.download_file_from_agent(cybServerGateway.DownloadFileFromAgentInputType(host='192.168.1.50',
+    code = cybServerGateway.generate_first_code_for_agent(cybServerGateway.GenerateFirstCodeForAgentInputType(ip_address='192.168.1.50'))
+    print(code)
+    cybServerGateway.download_file_from_agent(cybServerGateway.DownloadFileFromAgentInputType(ip_address='192.168.1.50',
                                                                                               file_path='/etc/passwd'))
