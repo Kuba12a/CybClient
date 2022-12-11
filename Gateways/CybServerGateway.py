0a28ef1a8e7e725a1ef9bf5d449313fb8260b7bb
kalus <kaluski1212@wp.pl>
2022-12-11 17:28:13 +0100

    Add logging command id

--- Gateways/CybServerGateway.py
+++ Gateways/CybServerGateway.py
@@ -28,10 +28,11 @@ def download_file_from_agent(input: DownloadFileFromAgentInputType):
     response = requests.post(url=HTTP_PREFIX+HOST+"/downloadFile", data=data)
 
     if response.status_code != 200:
-        Console.console.print(f"Couldn't schedule downloading file from agent {input.ip_address}", style="error")
+        Console.console.print(f"Could not schedule downloading file from agent {input.ip_address}", style="error")
         Console.console.print(response.json()['detail'], style="error")
     else:
-        Console.console.print('Task for downloading file from an agent scheduled successfully', style="success")
+        Console.console.print(f'Task for downloading file from an agent scheduled successfully with id:'
+                              f' {response.json()["command_id"]}', style="success")
 
     return response.json()
 
@@ -43,10 +44,12 @@ def list_files_from_agent(input: ListFilesFromAgentInputType):
     response = requests.post(url=HTTP_PREFIX+HOST+"/listFiles", data=data)
 
     if response.status_code != 200:
-        Console.console.print(f"Couldn't schedule listing files from agent {input.ip_address}", style="error")
+        Console.console.print(f"Could not schedule listing files from agent {input.ip_address}", style="error")
         Console.console.print(response.json()['detail'], style="error")
     else:
-        Console.console.print('Task for listing files from an agent scheduled successfully', style="success")
+        Console.console.print(f'Task for listing files from an agent scheduled successfully with id:'
+                              f' {response.json()["command_id"]}',
+                              style="success")
 
     return response.json()
 
